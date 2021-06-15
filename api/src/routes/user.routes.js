f769911fe9787a20a59e79ec7befb0a3a7f5650f
Eduardo-Lpz <jramos@arkusnexus.com>
2021-06-14 21:20:18 -0700

    verify token

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import users from '../controllers/user.controller';
-// import checkToken from '../middlewares/verify-token';
+import checkToken from '../middlewares/verify-token';
 
 const router = express.Router();
 
-// router.use(checkToken);
+router.use(checkToken);
 router.get('/', users.findAll);
 router.post('/', users.create);
 router.delete('/:id', users.deleteOne);
